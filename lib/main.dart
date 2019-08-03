9fd45276624b911c799199861e7b53867e727353
Benedict <benedictcontawe@gmail.com>
2019-08-04 03:49:09 +0800

    drawer

--- lib/main.dart
+++ lib/main.dart
@@ -1,56 +1,18 @@
+import 'pages/home_page.dart';
 import 'package:flutter/material.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: true,
-      title: 'Simple Toast App',
-      home: const Home(),
-    );
-  }
-}
-
-class Home extends StatelessWidget {
-  const Home({
-    Key key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Toast Title'),
+    return new MaterialApp(
+      title: 'NavigationDrawer Demo',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
       ),
-
-      /// We use [Builder] here to use a [context] that is a descendant of [Scaffold]
-      /// or else [Scaffold.of] will return null
-      body: Builder(
-        builder: (context) => Center(
-              child: RaisedButton(
-                child: const Text('Show Toast'),
-                onPressed: () => _showToast(context),
-              ),
-            ),
-      ),
+      home: new HomePage(),
     );
   }
-
-  void _showToast(BuildContext context) {
-    Fluttertoast.showToast(
-        msg: "This is Center Short Toast",
-        toastLength: Toast.LENGTH_SHORT,
-        gravity: ToastGravity.BOTTOM,
-        timeInSecForIos: 3,
-        backgroundColor: Colors.white,
-        textColor: Colors.black,
-        fontSize: 16.0
-    );
-  }
-  
 }
