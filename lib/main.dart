968664af39d2794bb202e715cb6319421adc8e05
Benedict <benedict.b.contawe@accenture.com>
2019-10-08 15:39:11 +0800

    Update code of main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
-//void main() => runApp(MyApp());
+void main() => runApp(MainApp());
 
-void main()
-{ 
+class MainApp extends StatelessWidget {
   //#region Constructor()
+  /*
   /*
   runApp(MaterialApp(
     title:"PageView",
@@ -31,7 +31,6 @@ void main()
   */
   //#endregion
   //#region PageView.Custom
-  /*
   runApp(MaterialApp(
     title:"PageView",
     home: Scaffold(
@@ -44,40 +43,41 @@ void main()
   */
   //#endregion
   PageController controller = PageController();
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: true,
-    title:"PageView",
-    home: Scaffold(
-      body: PageView(
-        children: <Widget>[
-          Container(
-            child: Center(child:Text("Page 1")),
-            color: Colors.red,
-          ),
-          Container(
-            child: Center(child:Text("Page 2")),
-            color: Colors.blueAccent,
-          ),
-          Container(
-            child: Center(child:Text("Page 3")),
-            color: Colors.amber,
-          ),
-          Container(
-            child: Center(child:Text("Page 4")),
-            color: Colors.greenAccent,
-          )
-        ],
-        controller: controller,
-        onPageChanged: (num){
-          //controller.jumpToPage(2);
-          print("Change: " + controller.position.toString());
-        },
-        scrollDirection: Axis.horizontal, // scrollDirection: Axis.vertical,
-        pageSnapping: true,
-        physics: ClampingScrollPhysics(), //physics: BouncingScrollPhysics(),
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: true,
+      title:"PageView",
+      home: Scaffold(
+        body: PageView(
+          children: <Widget>[
+            Container(
+              child: Center(child:Text("Page 1")),
+              color: Colors.red,
+            ),
+            Container(
+              child: Center(child:Text("Page 2")),
+              color: Colors.blueAccent,
+            ),
+            Container(
+              child: Center(child:Text("Page 3")),
+              color: Colors.amber,
+            ),
+            Container(
+              child: Center(child:Text("Page 4")),
+              color: Colors.greenAccent,
+            )
+          ],
+          controller: controller,
+          onPageChanged: (num){
+            //controller.jumpToPage(2);
+            print("Change: " + controller.position.toString());
+          },
+          scrollDirection: Axis.horizontal, // scrollDirection: Axis.vertical,
+          pageSnapping: true,
+          physics: ClampingScrollPhysics(), //physics: BouncingScrollPhysics(),
+        ),
       ),
-    ),
-  ));
+    );
+  }
 }
-
-//Reference: https://androidmonks.com/pageview-flutter/
