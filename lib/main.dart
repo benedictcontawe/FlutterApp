01c8ad0a40eede154382f9e6923452496511eecb
Benedict <benedict.b.contawe@accenture.com>
2019-10-16 14:53:03 +0800

    Rename MyApp to MainApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MainApp());
 }
 
-class MyApp extends StatelessWidget {
+class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
