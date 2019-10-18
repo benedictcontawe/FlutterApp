dce92de92ee32aa822c69c8c0eac7f54fb91cac7
Benedict <benedict.b.contawe@accenture.com>
2019-10-18 16:22:19 +0800

    rENAMe for packages folders

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:snack_bar/main.dart';
+import 'package:navigation_drawer_tab_bar/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
