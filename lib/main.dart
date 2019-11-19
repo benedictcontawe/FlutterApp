9fb8d56975097bb54929750cc6c157e75ff54697
Benedict <benedictcontawe@gmail.com>
2019-11-19 15:31:19 +0800

    Update with percentage layout using FractionallySizedBox, done ui for checkbox and functionality, done scroll.

--- lib/main.dart
+++ lib/main.dart
@@ -11,9 +11,10 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return  new MaterialApp(
       title: 'Scroll View',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(primarySwatch: Colors.teal),
       home: new ScrollViewWidget(),
-            );
+    );
   }  
 }
       
@@ -22,20 +23,96 @@ class ScrollViewWidget extends StatefulWidget {
 }
 
 class ScrollViewState extends State<ScrollViewWidget> {
+bool chckSwitch = false ?? false; //With null default value of false
+
+Widget customCheckBox() {
+  return FractionallySizedBox(
+    widthFactor: 1,
+    heightFactor: 0.1,
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Checkbox(
+          value: chckSwitch,
+          onChanged: (bool value) {
+            setState(() {
+              chckSwitch = value;
+            });
+          },
+        ),
+        Text("Scroll View Switch"),
+      ],
+    ),
+  );
+}
+
+Widget customScrollView() {
+  //TODO Create SingleChildScrollView, put all the long text inside
+  return FractionallySizedBox(
+    widthFactor: 1,
+    heightFactor: 0.9,
+    child: SingleChildScrollView(
+      physics: chckSwitch ? const  NeverScrollableScrollPhysics() : const AlwaysScrollableScrollPhysics(), //Conditional statement for enable and disable scrollview
+      child: Column(
+        children: <Widget>[
+          Container(color: Colors.red, height: 50.0),
+          Container(color: Colors.orange, height: 50.0),
+          Container(color: Colors.yellow, height: 50.0),
+          Container(color: Colors.green, height: 50.0),
+          Container(color: Colors.blue, height: 50.0),
+          Container(color: Colors.indigo, height: 50.0),
+          Container(color: Colors.purple, height: 50.0),
+          
+          Container(color: Colors.red, height: 50.0),
+          Container(color: Colors.orange, height: 50.0),
+          Container(color: Colors.yellow, height: 50.0),
+          Container(color: Colors.green, height: 50.0),
+          Container(color: Colors.blue, height: 50.0),
+          Container(color: Colors.indigo, height: 50.0),
+          Container(color: Colors.purple, height: 50.0),
+
+          Container(color: Colors.red, height: 50.0),
+          Container(color: Colors.orange, height: 50.0),
+          Container(color: Colors.yellow, height: 50.0),
+          Container(color: Colors.green, height: 50.0),
+          Container(color: Colors.blue, height: 50.0),
+          Container(color: Colors.indigo, height: 50.0),
+          Container(color: Colors.purple, height: 50.0),
+
+          Container(color: Colors.red, height: 50.0),
+          Container(color: Colors.orange, height: 50.0),
+          Container(color: Colors.yellow, height: 50.0),
+          Container(color: Colors.green, height: 50.0),
+          Container(color: Colors.blue, height: 50.0),
+          Container(color: Colors.indigo, height: 50.0),
+          Container(color: Colors.purple, height: 50.0),
+        ],
+      ),
+    ),
+  );
+}
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: null,
-      backgroundColor: Colors.black,
-      body: SingleChildScrollView(
-        child: new Stack(
+      appBar: AppBar(
+        title: const Text('ScrollViewApp'),
+      ),
+      backgroundColor: Colors.white,
+      body:  Stack (            
           fit: StackFit.expand,
           children: <Widget>[
-            //TODO Follow this https://github.com/benedictcontawe/ScrollViewApp
+            //Container(color: Colors.white,),
+            Align(
+              alignment: Alignment.topCenter,
+              child: customCheckBox(),
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: customScrollView(),
+            )
           ],
-        ),
-      )
+      ),
     );
   }
-
 }
