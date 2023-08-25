10e9b273b64428306a54fcf1f53bd96f4c8fe588
Benedict Contawe <benedictcontawe@gmail.com>
2023-08-25 19:01:58 +0800

    Initialize Firebase Classes

---
+++ lib/firebase/firebase_storage_service.dart
@@ -0,0 +1,7 @@
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/foundation.dart';
+import 'package:get/get_state_manager/src/rx_flutter/rx_disposable.dart';
+
+class FirebaseStorageService extends GetxService {
+
+}
---
+++ lib/firebase/firestore_service.dart
@@ -0,0 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:get/get.dart';
+
+class FirestoreService extends GetxService {
+
+}
