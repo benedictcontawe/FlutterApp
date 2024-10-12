7c2341afec8b3f4c669b2ca400e2a8fac24a5262
Ferer Atlus <benedictcontawe@gmail.com>
2024-10-12 22:30:29 +0800

    Major Update for Flutter GetX and Firebase

--- android/app/src/main/kotlin/com/sample/getx_storage/MainActivity.kt
+++ android/app/src/main/kotlin/com/sample/getx_storage/MainActivity.kt
@@ -2,5 +2,4 @@ package com.sample.getx_storage
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity: FlutterActivity()
--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/benedictcontawe/development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/benedictcontawe/Documents/Flutter Projects/FlutterApp"
+export "FLUTTER_APPLICATION_PATH=/Users/benedictcontawe/Documents/Flutter Projects/getx_storage"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
